fd01d561e31406e2141d93a0b71001c576ed4607
bhushan9 <bhushan.crysis@gmail.com>
2017-03-26 11:12:38 -0400

    Discretize data

--- Discretized.py
+++ Discretized.py
@@ -6,11 +6,36 @@ from sklearn.cluster import KMeans
 # Read the csv file
 df = pd.read_csv('MDP_Original_data.csv')
 # Start with 6th column and iterate till last column 
-for i in range(6,len(df.columns)):
+#for i in range(6,len(df.columns)):
 	#Use Kmeans to identify clusters
-	kmeans = KMeans(n_clusters=2, random_state=0).fit(df[df.columns[i]].values.reshape(-1,1))
-	#Assign the labels to existing data column
-	df[df.columns[i]] = kmeans.labels_ 
-	
 
-df.to_csv("discretized_output.csv", sep='\t', encoding='utf-8')
+a= ['Level','symbolicRepresentationCount','probIndexinLevel','TotalTime', 'AppCount', 'hintCount', 'probIndexinLevel', 'WrongApp']
+
+#	kmeans = KMeans(n_clusters=2, random_state=0).fit(df[df.columns[i]].values.reshape(-1,1))
+	#Assign the labels to existing data column
+#	df[df.columns[i]] = kmeans.labels_ 
+
+new_df = df.loc[:,df.columns[0] : df.columns[5]]
+
+
+
+for name in a:
+	new_df.loc[:,name] = df[name]
+
+
+#discretize the data (some features may need more than two levels)
+for i in range(9,len(new_df.columns)):
+
+	kmeans = KMeans(n_clusters=2, random_state=0).fit(new_df[new_df.columns[i]].values.reshape(-1,1))
+	new=[x=x+1 for x in kmeans.labels_]
+	new_df[new_df.columns[i]] = kmeans.labels_ 
+
+print(df.columns.get_loc("reward"))
+
+
+
+
+
+print(new_df.dtypes)
+
+new_df.to_csv("test.csv", sep=',')
