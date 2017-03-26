bc4354ece5bc67a8493cc62a7a8a0f51ad10102b
bhushan9 <bhushan.crysis@gmail.com>
2017-03-26 16:47:26 -0400

    finding cluster size wrt ECR

--- Discretized.py
+++ Discretized.py
@@ -2,14 +2,16 @@ import pandas as pd
 import numpy as np
 import scipy
 from sklearn.cluster import KMeans
+import os
+from copy import deepcopy
 
 # Read the csv file
 df = pd.read_csv('MDP_Original_data.csv')
 # Start with 6th column and iterate till last column 
 #for i in range(6,len(df.columns)):
-	#Use Kmeans to identify clusters
+	#Use Kmeans to identify cluster
 
-a= ['Level','symbolicRepresentationCount','probIndexinLevel','TotalTime', 'AppCount', 'hintCount', 'probIndexinLevel', 'WrongApp']
+a= ['Level','NewLevel','SolvedPSInLevel','probDiff','TotalTime','avgstepTimePS','RightApp']
 
 #	kmeans = KMeans(n_clusters=2, random_state=0).fit(df[df.columns[i]].values.reshape(-1,1))
 	#Assign the labels to existing data column
@@ -19,23 +21,62 @@ new_df = df.loc[:,df.columns[0] : df.columns[5]]
 
 
 
-for name in a:
-	new_df.loc[:,name] = df[name]
+# for name in a:
+# 	new_df.loc[:,name] = df[name]
 
+
+
+#new_df.loc[:,'f1']=4*df['cumul_difficultProblemCountSolved']+2*df['cumul_difficultProblemCountWE']+2*df['cumul_easyProblemCountSolved']+df['cumul_easyProblemCountWE']
+#new_df.loc[:,'f2'] = df['avgstepTime'] - df['avgstepTimePS']
 
 #discretize the data (some features may need more than two levels)
-for i in range(9,len(new_df.columns)):
 
-	kmeans = KMeans(n_clusters=2, random_state=0).fit(new_df[new_df.columns[i]].values.reshape(-1,1))
-	new=[x=x+1 for x in kmeans.labels_]
-	new_df[new_df.columns[i]] = kmeans.labels_ 
+values = []
+for i in range(7,8):
+	#test_df = deepcopy()
+	# test_df = deepcopy(new_df)
+	test_df = new_df
+	unique = len(np.unique(df[df.columns[i]]))
+	if unique >5 :
+		max_ecr = None
+		n = None
+		for no_of_clusters in range(3,6):
+			kmeans = KMeans(n_clusters=no_of_clusters, random_state=0).fit(df[df.columns[i]].values.reshape(-1,1))
+			test_df[df.columns[i]] = [x+1 for x in kmeans.labels_]
 
-print(df.columns.get_loc("reward"))
+			# Write to csv file 
+			test_df.to_csv("test.csv", sep=',')
+			cmd = "python MDP_process.py -input test.csv"
+			try:
+				ecr = float(os.popen(cmd).read().split('\n')[-2].split(': ')[1])
+			except:
+				ecr = None
+			if ecr is not None and (max_ecr is None or ecr>max_ecr):
+				max_ecr = ecr
+				n = no_of_clusters
+		values.append( str(df.columns[i]) + " : " + str(n) + " : " + str(max_ecr))
+
+print(values)
+
+# print os.popen(cmd).read().split('\n')[-1]
+# print float(os.popen(cmd).read().split('\n')[-2].split(': ')[1])
+		# print str(df.columns[i]) + " : Discretize Bitch! : " + str(unique)
+	# else:
+	# 	print str(df.columns[i]) + " : Am I not perfect?"
+		
+
+
+		 
+
+	
 
 
 
 
 
-print(new_df.dtypes)
+#calculate the chi square test
 
-new_df.to_csv("test.csv", sep=',')
+
+
+
+# new_df.to_csv("test.csv", sep=',')
