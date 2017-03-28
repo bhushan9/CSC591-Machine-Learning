388acbb190134e935f123c19606217ac1dceb429
bhushan9 <bhushan.crysis@gmail.com>
2017-03-28 00:01:59 -0400

    missing columns added

--- Discretized.py
+++ Discretized.py
@@ -19,7 +19,7 @@ a= ['Level','NewLevel','SolvedPSInLevel','probDiff','TotalTime','avgstepTimePS',
 
 new_df = df.loc[:,df.columns[0] : df.columns[5]]
 
-
+#store_df = new_df
 
 # for name in a:
 # 	new_df.loc[:,name] = df[name]
@@ -31,21 +31,26 @@ new_df = df.loc[:,df.columns[0] : df.columns[5]]
 
 #discretize the data (some features may need more than two levels)
 
-values = []
-for i in range(7,8):
+values_ecr = {} # stores max ecr value for a column
+name = []
+for i in range(7,10):
+	print(df.columns[i])
+
 	#test_df = deepcopy()
 	# test_df = deepcopy(new_df)
-	test_df = new_df
+	test_df = deepcopy(new_df)
+	#store_df = test_df
 	unique = len(np.unique(df[df.columns[i]]))
 	if unique >5 :
 		max_ecr = None
 		n = None
-		for no_of_clusters in range(3,6):
+		for no_of_clusters in range(2,6):
 			kmeans = KMeans(n_clusters=no_of_clusters, random_state=0).fit(df[df.columns[i]].values.reshape(-1,1))
 			test_df[df.columns[i]] = [x+1 for x in kmeans.labels_]
-
+			#name.append(len(test_df.columns))
 			# Write to csv file 
 			test_df.to_csv("test.csv", sep=',')
+			#print str('yolo') + str(test_df.columns)
 			cmd = "python MDP_process.py -input test.csv"
 			try:
 				ecr = float(os.popen(cmd).read().split('\n')[-2].split(': ')[1])
@@ -54,9 +59,20 @@ for i in range(7,8):
 			if ecr is not None and (max_ecr is None or ecr>max_ecr):
 				max_ecr = ecr
 				n = no_of_clusters
-		values.append( str(df.columns[i]) + " : " + str(n) + " : " + str(max_ecr))
+				test_df[df.columns[i]]=[x+1 for x in kmeans.labels_]
+				
+		#values.append( str(df.columns[i]) + " : " + str(n) + " : " + str(max_ecr))
+		values_ecr[df.columns[i]]=max_ecr
+	else:
+		test_df[df.columns[i]]=df[df.columns[i]]
+		
 
-print(values)
+test_df.to_csv("new.csv",sep=',')
+
+print(values_ecr)
+print(name)
+# write optimal discretized data to file 
+
 
 # print os.popen(cmd).read().split('\n')[-1]
 # print float(os.popen(cmd).read().split('\n')[-2].split(': ')[1])
